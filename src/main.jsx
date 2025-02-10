9221b22aacea628b287c8cd2a9571f9d34537fa8
Bons18 <brayanrpabon@gmail.com>
2025-02-10 07:49:37 -0500

    Se corriguen problemas de compatibildadv1

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./i18n/i18n"; // Importación del i18n configurado
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
